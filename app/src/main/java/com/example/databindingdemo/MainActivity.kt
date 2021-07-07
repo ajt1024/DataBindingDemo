066756fa8c4960b9da3721e6cbf89c02962bada6
Ang Jen Tat <angjt-wm18@student.tarc.edu.my>
2021-07-07 11:20:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/databindingdemo/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.databindingdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private var aPerson :Person = Person("ali","123456","ali@gmail.com","123 Jalan xzy")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.personData = aPerson
+
+//        binding.textView2.text = aPerson.name;
+//        binding.tvIC.text = aPerson.ic;
+//        binding.tvEmail.text = aPerson.email;
+//        binding.tvAddress.text = aPerson.address;
+
+        binding.btnUpdate.setOnClickListener(){
+            aPerson.email = "abu@gmail.com"
+            aPerson.address = "Jalan 123"
+            binding.apply { invalidateAll() }
+        }
+    }
+}
